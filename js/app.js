6c29606191fc0f3404686f7ea7da58042d58554b
이지은 <tori@ryanlab.kr>
2024-12-26 20:34:31 +0900

    refactor: script app.js 로 분절

--- js/app.js
+++ js/app.js
@@ -33,6 +33,209 @@ const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
 // ================================================
+// ================ 메인 페이지 시작 ================
+// ================================================
+
+$(document).ready(async () => {
+  // 멤버 카드 create
+  $("#saveBtn").click(async function () {
+    let name = $("#name").val();
+    let profileImg = $("#profileImg").val();
+    let intro = $("#intro").val();
+    let mbti = $("#mbti").val();
+    let merit = $("#merit").val();
+    let password = $("#password").val();
+
+    let doc = {
+      name: name,
+      profileImg: profileImg,
+      intro: intro,
+      mbti: mbti,
+      merit: merit,
+      password: password,
+    };
+
+    if (
+      name === "" ||
+      profileImg === "" ||
+      intro === "" ||
+      mbti === "" ||
+      merit === "" ||
+      password === ""
+    ) {
+      alert("빈칸을 모두 채워주세요!");
+      return;
+    }
+
+    await addDoc(collection(db, "members"), doc);
+    alert("저장완료!");
+    window.location.reload();
+  });
+
+  $("#closeBtn").click(async function () {
+    window.location.reload();
+  });
+
+  // 멤버 카드 read
+  let docs = await getDocs(collection(db, "members"));
+  $("#cards").empty(); // 기존 데이터를 초기화
+  docs.forEach(doc => {
+    let row = doc.data();
+    let id = doc.id; // 문서 ID
+    let name = row["name"];
+    let profileImg = row["profileImg"];
+    let intro = row["intro"];
+    let mbti = row["mbti"];
+    let merit = row["merit"];
+
+    let temp_html = `
+                  <div class="col">
+                    <a class="text-decoration-none" href="members_page.html?id=${id}&name=${encodeURIComponent(
+      name
+    )}&intro=${encodeURIComponent(intro)}&mbti=${encodeURIComponent(
+      mbti
+    )}&merit=${encodeURIComponent(merit)}&profileImg=${encodeURIComponent(
+      profileImg
+    )}">
+      <div class="card shadow-sm">
+        <img
+          class="bd-placeholder-img card-img-top"
+          width="100%"
+          height="225px"
+          src="${profileImg}"
+        >
+        </svg>
+        <div class="card-body">
+          <p class="card-text">
+              ${intro}
+          </p>
+        </div>
+      </div>
+    </a>
+  </div>
+`;
+    $("#cards").append(temp_html);
+  });
+});
+
+// ================================================
+// ================ 방명록 시작 ====================
+// ================================================
+
+// jQuery로 DOM이 준비되었을 때 실행
+$(document).ready(async () => {
+  // 방명록 남기기 버튼 클릭 이벤트
+  $("#btn_posting").click(async function () {
+    let username = $("#username").val();
+    let password = $("#password").val();
+    let message = $("#message").val();
+    let createdAt = new Date();
+
+    let doc = {
+      username: username,
+      password: password,
+      message: message,
+      createdAt: createdAt,
+    };
+    await addDoc(collection(db, "postings"), doc);
+
+    window.location.reload();
+  });
+
+  // 방명록 입력란 접기/펼치기
+  let boolean = true;
+  $("#btn_toggle").click(function () {
+    $("#postingbox").toggle();
+    if (boolean) {
+      $("#btn_toggle").text("펼치기");
+      boolean = false;
+    } else {
+      $("#btn_toggle").text("접기");
+      boolean = true;
+    }
+  });
+
+  // postings 컬렉션 읽어오기
+  const postingsRef = collection(db, "postings");
+  const q = query(postingsRef, orderBy("createdAt", "desc"));
+  let guestBooks = await getDocs(q);
+  guestBooks.forEach(book => {
+    let row = book.data();
+
+    let username = row["username"];
+    let message = row["message"];
+    let id = book.id;
+
+    let temp_html = `
+          <div class="myCard card-body shadow-sm rounded p-4">
+              <h5 class="card-title text-primary fw-bold">${username}</h5>
+              <p class="card-text text-muted">${message}</p>
+              <input class="cardId" type="hidden" value="${id}">
+              <div class="d-flex justify-content-end">
+                  <a href="#" class="btn btn-outline-primary btn-sm me-2 cardBtn cardUpdateBtn" data-bs-toggle="modal" data-bs-target="#passwordModal">수정하기</a>
+                  <a href="#" class="btn btn-outline-danger btn-sm cardBtn cardDeleteBtn" data-bs-toggle="modal" data-bs-target="#passwordModal">삭제하기</a>   
+              </div>
+          </div>`;
+    $("#guestBook").append(temp_html);
+  });
+
+  // 수정하기/삭제하기 버튼 클릭 이벤트 핸들러
+  $(".cardBtn").on("click", async function () {
+    let type;
+    if ($(this).hasClass("cardUpdateBtn")) type = "update";
+    else if ($(this).hasClass("cardDeleteBtn")) type = "delete";
+
+    let docId = $(this).parent().siblings(".cardId").val();
+
+    // 비밀번호 확인 버튼 클릭 이벤트 핸들러
+    $("#checkPasswordBtn").on("click", async function () {
+      let chkPw = $("#chkPw").val();
+
+      const docRef = doc(db, "postings", docId);
+      const docSnap = await getDoc(docRef);
+
+      if (docSnap.data().password == chkPw) {
+        // 수정인 경우
+        if (type == "update") {
+          $("#editUsername").val(docSnap.data().username);
+          $("#editMessage").val(docSnap.data().message);
+          cardUpdate(docId);
+        }
+        // 삭제인 경우
+        else if (type == "delete") {
+          cardDelete(docId);
+        }
+      } else {
+        alert("비밀번호가 틀립니다.");
+        return;
+      }
+    });
+  });
+
+  // 방명록 수정 함수
+  function cardUpdate(docId) {
+    $("#editModal").modal("show");
+    $("#saveMessageBtn").on("click", async function () {
+      let editMessage = $("#editMessage").val();
+      const docRef = doc(db, "postings", docId);
+      await updateDoc(docRef, {
+        message: editMessage,
+      });
+      alert("수정 완료");
+      window.location.reload();
+    });
+  }
+
+  // 방명록 삭제 함수
+  async function cardDelete(docId) {
+    const docRef = doc(db, "postings", docId);
+    await deleteDoc(docRef);
+    alert("삭제 완료");
+    window.location.reload();
+  }
+});
+
+// ================================================
 // ================ 멤버페이지 시작 ================
 // ================================================
 
