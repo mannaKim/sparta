3e8075c6155048990fa033c657edd73dbb855313
hanna <hannah.k0925@gmail.com>
2025-01-02 17:57:17 +0900

    feat: Lv 2. Calculator 클래스 필드 getter, setter 정의 및 활용

--- src/main/java/com/example/calculator2/App.java
+++ src/main/java/com/example/calculator2/App.java
@@ -22,18 +22,18 @@ public class App {
             System.out.print("사칙연산 기호를 입력하세요: ");
             char operator = sc.next().charAt(0);
 
-
             if (operator == '+' || operator == '-' || operator == '*' || operator == '/'
                 || operator == '➕' || operator == '➖' || operator == '✖' || operator == '➗') {
                 if ((operator == '/' || operator == '➗') && num2 == 0) {
                     System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
                 }
-                calculator.result = calculator.calculate(num1, num2, operator);
-                System.out.println("결과: " + calculator.result);
+                calculator.setResult(calculator.calculate(num1, num2, operator));
+                System.out.println("결과: " + calculator.getResult());
             }
             else {
                 System.out.println("사칙연산 기호가 적절하지 않습니다.");
             }
+
             // 반복의 종료를 알려주는 “exit” 문자열을 입력하기 전까지 무한으로 계산을 진행
             System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
             answer = sc.next();
--- src/main/java/com/example/calculator2/Calculator.java
+++ src/main/java/com/example/calculator2/Calculator.java
@@ -3,11 +3,13 @@ package com.example.calculator2;
 /*
 * 사칙연산을 수행 후, 결과값 반환 메서드 구현 & 연산 결과를 저장하는 컬렉션 타입 필드를 가진 Calculator 클래스를 생성
 * 1) 양의 정수 2개(0 포함)와 연산 기호를 매개변수로 받아 사칙연산(➕,➖,✖️,➗) 기능을 수행한 후
-  2) 결과 값을 반환하는 메서드와 연산 결과를 저장하는 컬렉션 타입 필드를 가진 Calculator 클래스를 생성합니다.
+* 2) 결과 값을 반환하는 메서드와 연산 결과를 저장하는 컬렉션 타입 필드를 가진 Calculator 클래스를 생성합니다.
+*
+* App 클래스의 main 메서드에서 Calculator 클래스의 연산 결과를 저장하고 있는 컬렉션 필드에 직접 접근하지 못하도록 수정 (캡슐화)
 */
 public class Calculator {
     // 연산 결과를 저장하는 컬렉션 타입 필드를 가진 Calculator 클래스를 생성
-    int result;
+    private int result;
 
     // 사칙연산을 수행한 후, 결과값을 반환하는 메서드 구현
     public int calculate(int num1, int num2, char op) {
@@ -18,5 +20,19 @@ public class Calculator {
             case '➗', '/' -> num1 / num2;
             default -> 0;
         };
+    }
+
+    /* Getter 메서드 구현 */
+    public int getResult() {
+        return this.result;
+    }
+
+    /* Setter 메서드 구현 */
+    public void setResult(int r) {
+        this.result = r;
+    }
+
+    public void removeResult() {
+        /* 구현 */
     }
 }
