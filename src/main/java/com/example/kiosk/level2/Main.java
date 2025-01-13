569899cae1cc7ea8091f2e284483ea9ea6ef48c4
hanna <hannah.k0925@gmail.com>
2025-01-13 20:38:33 +0900

    chore: Add Lv2 package and initial class files

---
+++ src/main/java/com/example/kiosk/level2/Main.java
@@ -0,0 +1,7 @@
+package com.example.kiosk.level2;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/com/example/kiosk/level2/MenuItem.java
@@ -0,0 +1,9 @@
+package com.example.kiosk.level2;
+/*
+* MenuItem: 세부 메뉴 속성 가지는 클래스
+* 햄버거의 이름, 가격설명
+* 예시: ShackBurger, 6.9, 토마토, 양상추, 쉑소스가 토핑된 치즈버거
+* new MenuItem("ShackBurger", 6.9, "토마토, 양상추, 쉑소스가 토핑된 치즈버거")
+*/
+public class MenuItem {
+}
