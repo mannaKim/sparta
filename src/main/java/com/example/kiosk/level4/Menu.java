1c1ef9ed2a3998a67dcd8630d8a49f301ab95517
hanna <hannah.k0925@gmail.com>
2025-01-14 15:11:54 +0900

    chore: Add Lv4 package and initial class files

---
+++ src/main/java/com/example/kiosk/level4/Menu.java
@@ -0,0 +1,7 @@
+package com.example.kiosk.level4;
+/*
+* MenuItem 클래스를 관리하는 클래스
+* 예시 : 버거 메뉴, 음료 메뉴 등 각 카테고리 내에 여러 MenuItem을 포함합니다.
+*/
+public class Menu {
+}
---
+++ src/main/java/com/example/kiosk/level4/MenuItem.java
@@ -0,0 +1,4 @@
+package com.example.kiosk.level4;
+
+public class MenuItem {
+}
