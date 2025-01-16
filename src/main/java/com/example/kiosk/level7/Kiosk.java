3eda5c6b3a26af341a48c403346ec3983c296d20
hanna <hannah.k0925@gmail.com>
2025-01-16 21:49:39 +0900

    chore: Add Lv7 package and initial class files and copy Lv6 classes

---
+++ src/main/java/com/example/kiosk/level7/Kiosk.java
@@ -0,0 +1,179 @@
+package com.example.kiosk.level7;
+
+import com.example.kiosk.exceptions.InvalidMenuSelectionException;
+
+import java.util.List;
+import java.util.Scanner;
+
+/*
+ * Kiosk: 프로그램 순서 및 흐름 제어를 담당하는 클래스
+ * 설명: 키오스크 프로그램의 메뉴를 관리하고 사용자 입력을 처리하는 클래스입니다.
+ */
+public class Kiosk {
+    private final List<Menu> menuList;
+    private Cart cart;
+
+    public Kiosk(List<Menu> menuList) {
+        this.menuList = menuList;
+    }
+
+    public void start() {
+        Scanner sc = new Scanner(System.in);
+        cart = new Cart();
+
+        while (true) {
+            // 카테고리 출력
+            Menu.printCategoryMenu(menuList);
+
+            // Order 메뉴(주문하기/취소하기) 출력
+            int categoryCount = menuList.size();
+            int additionalOrderMenuCount = 2;
+            int totalMenuCount = categoryCount + additionalOrderMenuCount;
+            boolean canOrder = false;
+            if (!cart.getCartItemMap().isEmpty()) {
+                cart.printOrderMenu(categoryCount);
+                canOrder = true;
+            }
+
+            String selectedMenu = sc.next();
+            try {
+                if ("0".equals(selectedMenu)) break;
+
+                int selectedMenuNumber = Integer.parseInt(selectedMenu);
+                if (!canOrder) {
+                    if (selectedMenuNumber > 0 && selectedMenuNumber <= categoryCount) {
+                        startViewSelectedCategoryMenu(selectedMenuNumber - 1);
+                    }
+                    else {
+                        throw new InvalidMenuSelectionException(0, categoryCount);
+                    }
+                }
+                else {
+                    if (selectedMenuNumber > 0 && selectedMenuNumber <= categoryCount) {
+                        startViewSelectedCategoryMenu(selectedMenuNumber - 1);
+                    }
+                    else if (selectedMenuNumber > 0 && selectedMenuNumber <= totalMenuCount) {
+                        if (selectedMenuNumber == totalMenuCount) {
+                            startCancelOrder();
+                        }
+                        else {
+                            startPlaceOrder();
+                        }
+                    }
+                    else {
+                        throw new InvalidMenuSelectionException(0, totalMenuCount);
+                    }
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("\"" + selectedMenu + "\"은 숫자가 아닙니다. 숫자를 다시 입력하세요.");
+            } catch (InvalidMenuSelectionException e) {
+                System.out.println(e.getMessage());
+            } catch (Exception e) {
+                System.out.println("알 수 없는 오류가 발생했습니다. : " + e.getMessage());
+            }
+            System.out.println();
+        }
+        System.out.println("프로그램을 종료합니다.");
+    }
+
+    private void startViewSelectedCategoryMenu(int index) {
+        // 메뉴 출력
+        menuList.get(index).printMenuItems();
+
+        Scanner sc = new Scanner(System.in);
+        String selectedMenuItem = sc.next();
+        try {
+            if ("0".equals(selectedMenuItem)) return;
+            int selectedMenuItemNumber = Integer.parseInt(selectedMenuItem);
+            int menuItemsCount = menuList.get(index).getMenuItems().size();
+            if (selectedMenuItemNumber > 0 && selectedMenuItemNumber <= menuItemsCount) {
+                // menuItems 중 선택된 menuItem 출력
+                this.menuList.get(index).printSelectedMenuItem(selectedMenuItemNumber - 1);
+                // 해당 item 장바구니에 담기
+                startAddToCart(menuList.get(index).getMenuItem(selectedMenuItemNumber - 1));
+            } else {
+                throw new InvalidMenuSelectionException(0, menuItemsCount);
+            }
+        } catch (NumberFormatException e) {
+            System.out.println("\"" + selectedMenuItem + "\"은 숫자가 아닙니다. 숫자를 다시 입력하세요.");
+        } catch (InvalidMenuSelectionException e) {
+            System.out.println(e.getMessage());
+        } catch (Exception e) {
+            System.out.println("알 수 없는 오류가 발생했습니다. : " + e.getMessage());
+        }
+    }
+
+    private void startAddToCart(MenuItem item) {
+        final int ADD_TO_CART_OPTION = 1; // 장바구니에 추가
+        final int CANCEL_OPTION = 2;      // 취소
+
+        System.out.println("\n\"" + MenuItem.formatMenuItem(item) + "\"");
+        System.out.println("위 메뉴를 장바구니에 추가하시겠습니까?");
+        System.out.println("1. 확인\t2. 취소");
+
+        Scanner sc = new Scanner(System.in);
+        String addToCartChoice = sc.next();
+
+        try {
+            int addToCartChoiceNumber = Integer.parseInt(addToCartChoice);
+
+            if (addToCartChoiceNumber == ADD_TO_CART_OPTION) {
+                cart.addCartItem(item);
+                System.out.println("\n"+ item.getMenuName() + " 이 장바구니에 추가되었습니다.");
+                System.out.println("\n아래 메뉴판을 보시고 메뉴를 골라 입력해주세요.");
+            }
+            else if (addToCartChoiceNumber == CANCEL_OPTION) {
+                System.out.println("\n취소되었습니다.");
+                System.out.println("\n아래 메뉴판을 보시고 메뉴를 골라 입력해주세요.");
+            }
+            else {
+                throw new InvalidMenuSelectionException(ADD_TO_CART_OPTION, CANCEL_OPTION);
+            }
+        } catch (NumberFormatException e) {
+            System.out.println("\"" + addToCartChoice + "\"은/는 숫자가 아닙니다.");
+        } catch (InvalidMenuSelectionException e) {
+            System.out.println(e.getMessage());
+        } catch (Exception e) {
+            System.out.println("알 수 없는 오류가 발생했습니다. : " + e.getMessage());
+        }
+    }
+
+    private void startPlaceOrder() {
+        final int PLACE_ORDER_OPTION = 1;       // 주문하기
+        final int RETURN_TO_MENU_OPTION = 2;    // 메뉴판으로 돌아가기
+
+        System.out.println("\n아래와 같이 주문하시겠습니까?");
+        cart.printCartItemsWithTotal();
+        System.out.println("1. 주문\t2. 메뉴판");
+
+        Scanner sc = new Scanner(System.in);
+        String placeOrderChoice = sc.next();
+
+        try {
+            int placeOrderChoiceNumber = Integer.parseInt(placeOrderChoice);
+
+            if (placeOrderChoiceNumber == PLACE_ORDER_OPTION) {
+                cart.placeOrder();
+            }
+            else if (placeOrderChoiceNumber == RETURN_TO_MENU_OPTION) {
+                System.out.println("\n메뉴판으로 돌아갑니다.");
+                System.out.println("\n아래 메뉴판을 보시고 메뉴를 골라 입력해주세요.");
+            }
+            else {
+                throw new InvalidMenuSelectionException(PLACE_ORDER_OPTION, RETURN_TO_MENU_OPTION);
+            }
+        } catch (NumberFormatException e) {
+            System.out.println("\"" + placeOrderChoice + "\"은/는 숫자가 아닙니다.");
+        } catch (InvalidMenuSelectionException e) {
+            System.out.println(e.getMessage());
+        } catch (Exception e) {
+            System.out.println("알 수 없는 오류가 발생했습니다. : " + e.getMessage());
+        }
+    }
+
+    private void startCancelOrder() {
+        cart.cancelOrder();
+        System.out.println("\n전체 주문이 취소되어 처음으로 돌아갑니다.");
+        System.out.println("\n아래 메뉴판을 보시고 메뉴를 골라 입력해주세요.");
+    }
+}
---
+++ src/main/java/com/example/kiosk/level7/MenuItem.java
@@ -0,0 +1,55 @@
+package com.example.kiosk.level7;
+
+import java.util.Objects;
+
+/*
+ * MenuItem: 세부 메뉴 속성 가지는 클래스
+ * 햄버거의 이름, 가격설명
+ * 예시: ShackBurger, 6.9, 토마토, 양상추, 쉑소스가 토핑된 치즈버거
+ * new MenuItem("ShackBurger", 6.9, "토마토, 양상추, 쉑소스가 토핑된 치즈버거")
+ */
+public class MenuItem {
+    private final String menuName;
+    private final double menuPrice;
+    private final String menuDescription;
+
+    public MenuItem (String menuName, double menuPrice, String menuDescription) {
+        this.menuName = menuName;
+        this.menuPrice = menuPrice;
+        this.menuDescription = menuDescription;
+    }
+
+    public String getMenuName() {
+        return menuName;
+    }
+
+    public double getMenuPrice() {
+        return menuPrice;
+    }
+
+    public String getMenuDescription() {
+        return menuDescription;
+    }
+
+    // menuItem의 String format을 반환하는 static 함수
+    public static String formatMenuItem(MenuItem item) {
+        return String.format(
+                "%-20s\t | W %.1f\t | %s",
+                item.getMenuName(),
+                item.getMenuPrice(),
+                item.getMenuDescription()
+        );
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        MenuItem menuItem = (MenuItem) o;
+        return Double.compare(menuPrice, menuItem.menuPrice) == 0 && Objects.equals(menuName, menuItem.menuName) && Objects.equals(menuDescription, menuItem.menuDescription);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(menuName, menuPrice, menuDescription);
+    }
+}
