87cf900b40f18ecf4c7afb6663d0e5f9be620264
hanna <hannah.k0925@gmail.com>
2025-01-23 15:33:43 +0900

    refactor: Implement Layered Architecture and refactor "spring-basic-crud"

---
+++ spring-basic-layered/src/main/java/com/example/layered/SpringBasicLayeredApplication.java
@@ -0,0 +1,13 @@
+package com.example.layered;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBasicLayeredApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBasicLayeredApplication.class, args);
+    }
+
+}
---
+++ spring-basic-layered/src/test/java/com/example/layered/SpringBasicLayeredApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.layered;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBasicLayeredApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
