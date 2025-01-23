59fe122c815473ad7f33c30a06407625808855a4
hanna <hannah.k0925@gmail.com>
2025-01-23 13:41:20 +0900

    fix: Resolve miscommunication and incorrect handling In an in-memory environment

--- spring-basic-crud/src/main/java/com/example/memo/controller/MemoController.java
+++ spring-basic-crud/src/main/java/com/example/memo/controller/MemoController.java
@@ -3,11 +3,11 @@ package com.example.memo.controller;
 import com.example.memo.dto.MemoRequestDto;
 import com.example.memo.dto.MemoResponseDto;
 import com.example.memo.entity.Memo;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 @RestController
 @RequestMapping("/memos")
@@ -15,7 +15,7 @@ public class MemoController {
     private final Map<Long, Memo> memoList = new HashMap<>();
 
     @PostMapping
-    public MemoResponseDto createMemo(@RequestBody MemoRequestDto dto) {
+    public ResponseEntity<MemoResponseDto> createMemo(@RequestBody MemoRequestDto dto) {
 
         // 식별자가 1씩 증가하도록 만들어야함
         Long memoId = memoList.isEmpty() ? 1 : Collections.max(memoList.keySet()) + 1;
@@ -26,32 +26,102 @@ public class MemoController {
         // Inmemory DB에 Memo 저장
         memoList.put(memoId, memo);
 
-        return new MemoResponseDto(memo);
+        return new ResponseEntity<>(new MemoResponseDto(memo), HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<MemoResponseDto>> findAllMemos() {
+
+        // init List
+        List<MemoResponseDto> responseList = new ArrayList<>();
+
+        // HashMap<Memo> -> List<MemoResponseDto>
+        // Map To List 1번째 방법
+//        for (Memo memo : memoList.values()) {
+//            MemoResponseDto responseDto = new MemoResponseDto(memo);
+//            responseList.add(responseDto);
+//        }
+
+        // Map To List 2번째 방법
+        responseList = memoList.values().stream().map(MemoResponseDto::new).toList();
+
+        if (responseList.isEmpty()) {
+            // 요청은 성공했으나, 응답 본문 없음\
+            // ResponseEntity는 제네릭 타입과 관계없이 body를 생략하거나 null로 처리하는 것을 허용
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            // 이 if문이 없다면, responseList가 비어있을 때 200OK와 빈 배열을 반환한다.
+        }
+        return new ResponseEntity<>(responseList, HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
-    public MemoResponseDto findMemoById(@PathVariable Long id) {
+    public ResponseEntity<MemoResponseDto> findMemoById(@PathVariable Long id) {
 
         Memo memo = memoList.get(id);
+        
+        if (memo == null) {
+            // 조회된 메모가 없을 때 404 not found 반환
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
 
-        return new MemoResponseDto(memo);
+        return new ResponseEntity<>(new MemoResponseDto(memo), HttpStatus.OK);
     }
 
     @PutMapping("/{id}")
-    public MemoResponseDto updateMemoById(
+    public ResponseEntity<MemoResponseDto> updateMemoById(
             @PathVariable Long id,
             @RequestBody MemoRequestDto dto
     ) {
         Memo memo = memoList.get(id);
 
+        if (memo == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        // 필수값 검증: 제목과 내용
+        if (dto.getTitle() == null || dto.getContents() == null) {
+            // 필수값이 없을 때 400 bad request 반환
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
         memo.update(dto);
 
-        return new MemoResponseDto(memo);
+        return new ResponseEntity<>(new MemoResponseDto(memo), HttpStatus.OK);
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<MemoResponseDto> updateTitle(
+        @PathVariable Long id,
+        @RequestBody MemoRequestDto dto
+    ) {
+        Memo memo = memoList.get(id);
+
+        // NPE 방지
+        if (memo == null) {
+            // 조회된 메모가 없을 때 404 not found 반환
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        // 필수값 검증: 제목 (내용은 없어야만 함)
+        if (dto.getTitle() == null || dto.getContents() != null) {
+            // 필수값이 없을 때 400 bad request 반환
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+        memo.updateTitle(dto);
+
+        return new ResponseEntity<>(new MemoResponseDto(memo), HttpStatus.OK);
     }
 
     @DeleteMapping("/{id}")
-    public void deleteMemo(@PathVariable long id) {
+    public ResponseEntity<Void> deleteMemo(@PathVariable long id) {
 
-        memoList.remove(id);
+        // memoList의 Key값에 id를 포함하고 있다면 삭제
+        if (memoList.containsKey(id)) {
+            memoList.remove(id);
+
+            return new ResponseEntity<>(HttpStatus.OK);
+        }
+
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 }
