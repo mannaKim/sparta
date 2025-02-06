99e069b3f1784c36c20bba827f443561a5261050
hanna <hannah.k0925@gmail.com>
2025-02-06 17:56:43 +0900

    feat: Member 비밀번호 수정 API 구현

--- spring-jpa-board/src/main/java/com/example/board/service/MemberService.java
+++ spring-jpa-board/src/main/java/com/example/board/service/MemberService.java
@@ -7,6 +7,7 @@ import com.example.board.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.Optional;
@@ -43,5 +44,17 @@ public class MemberService {    // 확장될 가능성이 없다면 Interface가
                 findMember.getUserName(),
                 findMember.getAge()
         );
+    }
+
+    @Transactional
+    public void updatePassword(Long id, String oldPassword, String newPassword) {
+
+        Member findMember = memberRepository.findByIdOrElseThrow(id);
+
+        if (!findMember.getPassword().equals(oldPassword)) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "비밀번호가 일치하지 않습니다.");
+        }
+
+        findMember.updatePassword(newPassword);
     }
 }
