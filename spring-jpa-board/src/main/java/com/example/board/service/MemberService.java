ffa658bd2d9bc51cbf330125f89d5ca16d4f454e
hanna <hannah.k0925@gmail.com>
2025-02-06 17:44:45 +0900

    feat: 특정 Member 조회 API 구현

--- spring-jpa-board/src/main/java/com/example/board/service/MemberService.java
+++ spring-jpa-board/src/main/java/com/example/board/service/MemberService.java
@@ -1,10 +1,15 @@
 package com.example.board.service;
 
+import com.example.board.dto.MemberResponseDto;
 import com.example.board.dto.SignUpResponseDto;
 import com.example.board.entity.Member;
 import com.example.board.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -22,5 +27,21 @@ public class MemberService {    // 확장될 가능성이 없다면 Interface가
                 savedMember.getId(),
                 savedMember.getUserName(),
                 savedMember.getAge());
+    }
+
+    public MemberResponseDto findById(Long id) {
+
+        Optional<Member> optionalMember = memberRepository.findById(id);
+
+        if (optionalMember.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Dose not exist id: " + id);
+        }
+
+        Member findMember = optionalMember.get();
+
+        return new MemberResponseDto(
+                findMember.getUserName(),
+                findMember.getAge()
+        );
     }
 }
